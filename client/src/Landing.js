834ccabd60acea02cc46a92173d682d5a804b16e
Mohamed <97177001+Mohamed110011@users.noreply.github.com>
2024-07-10 13:02:39 +0100

    try to fix landing

--- client/src/Landing.js
+++ client/src/Landing.js
@@ -3,7 +3,10 @@ import { Link } from "react-router-dom";
 import './assets/css/agency.min.css';
 import './assets/css/bootstrap.min.css';
 import './App.css';
+import './assets/css/style.css';
 import Portfolio from './components/Portfolio'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faSearch, faShoppingCart, faSignIn, faBars } from '@fortawesome/free-solid-svg-icons';
 
 
 
@@ -12,403 +15,70 @@ import Portfolio from './components/Portfolio'
 
 const Landing = () => {
   return (
-    <div className="App">
-      <nav className="navbar navbar-expand-lg navbar-dark fixed-top" id="mainNav">
-    <div className="container">
-      <a className="navbar-brand js-scroll-trigger" href="#page-top">Start Bootstrap</a>
-      <button className="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
-        Menu
-        <i className="fa fa-bars"></i>
-      </button>
-      <div className="collapse navbar-collapse" id="navbarResponsive">
-        <ul className="navbar-nav text-uppercase ml-auto">
-          <li className="nav-item">
-            <a className="nav-link js-scroll-trigger" href="#services">Services</a>
-          </li>
-          <li className="nav-item">
-              <Link className="nav-link" to="/login">Login</Link>
-              </li>
-          <li className="nav-item">
-            <a className="nav-link js-scroll-trigger" href="#about">About</a>
-          </li>
-          <li className="nav-item">
-            <a className="nav-link js-scroll-trigger" href="#team">Team</a>
-          </li>
-          <li className="nav-item">
-            <a className="nav-link js-scroll-trigger" href="#contact">Contact</a>
-          </li>
+    <div className="main-container">
+      <div className="background"></div>
+      <img src="assets/images/decor.png" alt="" className="decor" />
+
+      <div className="navbar">
+        <a href="#" className="main-logo">
+          <img src="assets/images/logo.svg" alt="Main Logo" />
+        </a>
+        <a href="#" className="reslogo">
+          <img src="assets/images/res-logo.svg" alt="Restaurant Logo" />
+        </a>
+
+        <ul>
+          <li><a href="#" className="active">Home</a></li>
+          <li><a href="#">Menu</a></li>
+          <li><a href="#">Service</a></li>
+          <li><a href="#">Shop</a></li>
         </ul>
-      </div>
-    </div>
-  </nav>
 
-  
-  <header className="masthead">
-    <div className="container">
-      <div className="intro-text">
-        <div className="intro-lead-in">Welcome To Our Studio!</div>
-        <div className="intro-heading text-uppercase">It's Nice To Meet You</div>
-        <a className="btn btn-primary btn-xl text-uppercase js-scroll-trigger" href="#services">Tell Me More</a>
-      </div>
-    </div>
-  </header>
-
-  
-  <section className="page-section" id="services">
-    <div className="container">
-      <div className="row">
-        <div className="col-lg-12 text-center">
-          <h2 className="section-heading text-uppercase">Services</h2>
-          <h3 className="section-subheading text-muted">Lorem ipsum dolor sit amet consectetur.</h3>
-        </div>
-      </div>
-      <div className="row text-center">
-        <div className="col-md-4">
-          <span className="fa-stack fa-4x">
-            <i className="fa fa-circle fa-stack-2x text-primary"></i>
-            <i className="fa fa-shopping-cart fa-stack-1x fa-inverse"></i>
-          </span>
-          <h4 className="service-heading">E-Commerce</h4>
-          <p className="text-muted">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minima maxime quam architecto quo inventore harum ex magni, dicta impedit.</p>
-        </div>
-        <div className="col-md-4">
-          <span className="fa-stack fa-4x">
-            <i className="fa fa-circle fa-stack-2x text-primary"></i>
-            <i className="fa fa-laptop fa-stack-1x fa-inverse"></i>
-          </span>
-          <h4 className="service-heading">Responsive Design</h4>
-          <p className="text-muted">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minima maxime quam architecto quo inventore harum ex magni, dicta impedit.</p>
-        </div>
-        <div className="col-md-4">
-          <span className="fa-stack fa-4x">
-            <i className="fa fa-circle fa-stack-2x text-primary"></i>
-            <i className="fa fa-lock fa-stack-1x fa-inverse"></i>
-          </span>
-          <h4 className="service-heading">Web Security</h4>
-          <p className="text-muted">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minima maxime quam architecto quo inventore harum ex magni, dicta impedit.</p>
-        </div>
-      </div>
-    </div>
-  </section>
-
-  
-
-  
-  <section className="page-section" id="about">
-    <div className="container">
-      <div className="row">
-        <div className="col-lg-12 text-center">
-          <h2 className="section-heading text-uppercase">About</h2>
-          <h3 className="section-subheading text-muted">Lorem ipsum dolor sit amet consectetur.</h3>
-        </div>
-      </div>
-      <div className="row">
-        <div className="col-lg-12">
-          <ul className="timeline">
-            <li>
-              <div className="timeline-image">
-                <img className="rounded-circle img-fluid" src="img/about/1.jpg" alt=""/>
-              </div>
-              <div className="timeline-panel">
-                <div className="timeline-heading">
-                  <h4>2009-2011</h4>
-                  <h4 className="subheading">Our Humble Beginnings</h4>
-                </div>
-                <div className="timeline-body">
-                  <p className="text-muted">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sunt ut voluptatum eius sapiente, totam reiciendis temporibus qui quibusdam, recusandae sit vero unde, sed, incidunt et ea quo dolore laudantium consectetur!</p>
-                </div>
-              </div>
-            </li>
-            <li className="timeline-inverted">
-              <div className="timeline-image">
-                <img className="rounded-circle img-fluid" src="img/about/2.jpg" alt=""/>
-              </div>
-              <div className="timeline-panel">
-                <div className="timeline-heading">
-                  <h4>March 2011</h4>
-                  <h4 className="subheading">An Agency is Born</h4>
-                </div>
-                <div className="timeline-body">
-                  <p className="text-muted">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sunt ut voluptatum eius sapiente, totam reiciendis temporibus qui quibusdam, recusandae sit vero unde, sed, incidunt et ea quo dolore laudantium consectetur!</p>
-                </div>
-              </div>
-            </li>
-            <li>
-              <div className="timeline-image">
-                <img className="rounded-circle img-fluid" src="img/about/3.jpg" alt=""/>
-              </div>
-              <div className="timeline-panel">
-                <div className="timeline-heading">
-                  <h4>December 2012</h4>
-                  <h4 className="subheading">Transition to Full Service</h4>
-                </div>
-                <div className="timeline-body">
-                  <p className="text-muted">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sunt ut voluptatum eius sapiente, totam reiciendis temporibus qui quibusdam, recusandae sit vero unde, sed, incidunt et ea quo dolore laudantium consectetur!</p>
-                </div>
-              </div>
-            </li>
-            <li className="timeline-inverted">
-              <div className="timeline-image">
-                <img className="rounded-circle img-fluid" src="img/about/4.jpg" alt=""/>
-              </div>
-              <div className="timeline-panel">
-                <div className="timeline-heading">
-                  <h4>July 2014</h4>
-                  <h4 className="subheading">Phase Two Expansion</h4>
-                </div>
-                <div className="timeline-body">
-                  <p className="text-muted">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sunt ut voluptatum eius sapiente, totam reiciendis temporibus qui quibusdam, recusandae sit vero unde, sed, incidunt et ea quo dolore laudantium consectetur!</p>
-                </div>
-              </div>
-            </li>
-            <li className="timeline-inverted">
-              <div className="timeline-image">
-                <h4>Be Part
-                  <br/>Of Our
-                  <br/>Story!</h4>
-              </div>
-            </li>
-          </ul>
-        </div>
-      </div>
-    </div>
-  </section>
-
-  
-  <section className="bg-light page-section" id="team">
-    <div className="container">
-      <div className="row">
-        <div className="col-lg-12 text-center">
-          <h2 className="section-heading text-uppercase">Our Amazing Team</h2>
-          <h3 className="section-subheading text-muted">Lorem ipsum dolor sit amet consectetur.</h3>
-        </div>
-      </div>
-      <div className="row">
-        <div className="col-sm-4">
-          <div className="team-member">
-            <img className="mx-auto rounded-circle" src="img/team/1.jpg" alt=""/>
-            <h4>Kay Garland</h4>
-            <p className="text-muted">Lead Designer</p>
-            <ul className="list-inline social-buttons">
-              <li className="list-inline-item">
-                <a href="#something">
-                  <i className="fa fa-twitter"></i>
-                </a>
-              </li>
-              <li className="list-inline-item">
-                <a href="#something">
-                  <i className="fa fa-facebook-f"></i>
-                </a>
-              </li>
-              <li className="list-inline-item">
-                <a href="#something">
-                  <i className="fa fa-linkedin-in"></i>
-                </a>
-              </li>
-            </ul>
-          </div>
-        </div>
-        <div className="col-sm-4">
-          <div className="team-member">
-            <img className="mx-auto rounded-circle" src="img/team/2.jpg" alt=""/>
-            <h4>Larry Parker</h4>
-            <p className="text-muted">Lead Marketer</p>
-            <ul className="list-inline social-buttons">
-              <li className="list-inline-item">
-                <a href="#something">
-                  <i className="fa fa-twitter"></i>
-                </a>
-              </li>
-              <li className="list-inline-item">
-                <a href="#something">
-                  <i className="fa fa-facebook-f"></i>
-                </a>
-              </li>
-              <li className="list-inline-item">
-                <a href="#something">
-                  <i className="fa fa-linkedin-in"></i>
-                </a>
-              </li>
-            </ul>
-          </div>
-        </div>
-        <div className="col-sm-4">
-          <div className="team-member">
-            <img className="mx-auto rounded-circle" src="img/team/3.jpg" alt=""/>
-            <h4>Diana Pertersen</h4>
-            <p className="text-muted">Lead Developer</p>
-            <ul className="list-inline social-buttons">
-              <li className="list-inline-item">
-                <a href="#something">
-                  <i className="fa fa-twitter"></i>
-                </a>
-              </li>
-              <li className="list-inline-item">
-                <a href="#something">
-                  <i className="fa fa-facebook-f"></i>
-                </a>
-              </li>
-              <li className="list-inline-item">
-                <a href="#something">
-                  <i className="fa fa-linkedin-in"></i>
-                </a>
-              </li>
-            </ul>
-          </div>
-        </div>
-      </div>
-      <div className="row">
-        <div className="col-lg-8 mx-auto text-center">
-          <p className="large text-muted">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aut eaque, laboriosam veritatis, quos non quis ad perspiciatis, totam corporis ea, alias ut unde.</p>
-        </div>
-      </div>
-    </div>
-  </section>
-
-  
-  <section className="py-5">
-    <div className="container">
-      <div className="row">
-        <div className="col-md-3 col-sm-6">
-          <a href="#something">
-            <img className="img-fluid d-block mx-auto" src="img/logos/envato.jpg" alt=""/>
+        <div className="nav-icon">
+          <a href="#">
+            <FontAwesomeIcon icon={faSearch} aria-hidden="true" />
+          </a>
+          <a href="#">
+            <FontAwesomeIcon icon={faShoppingCart} aria-hidden="true" />
           </a>
         </div>
-        <div className="col-md-3 col-sm-6">
-          <a href="#something">
-            <img className="img-fluid d-block mx-auto" src="img/logos/designmodo.jpg" alt=""/>
-          </a>
-        </div>
-        <div className="col-md-3 col-sm-6">
-          <a href="#something">
-            <img className="img-fluid d-block mx-auto" src="img/logos/themeforest.jpg" alt=""/>
-          </a>
-        </div>
-        <div className="col-md-3 col-sm-6">
-          <a href="#something">
-            <img className="img-fluid d-block mx-auto" src="img/logos/creative-market.jpg" alt=""/>
-          </a>
-        </div>
-      </div>
-    </div>
-  </section>
 
-  
-  <section className="page-section" id="contact">
-    <div className="container">
-      <div className="row">
-        <div className="col-lg-12 text-center">
-          <h2 className="section-heading text-uppercase">Contact Us</h2>
-          <h3 className="section-subheading text-muted">Lorem ipsum dolor sit amet consectetur.</h3>
+        <div className="nav-btn">
+          <button>
+            <FontAwesomeIcon icon={faSignIn} aria-hidden="true" />
+            Sign In
+          </button>
+          <button>
+            <FontAwesomeIcon icon={faSignIn} aria-hidden="true" />
+            Sign Up
+          </button>
         </div>
-      </div>
-      <div className="row">
-        <div className="col-lg-12">
-          <form id="contactForm" name="sentMessage" novalidate="novalidate">
-            <div className="row">
-              <div className="col-md-6">
-                <div className="form-group">
-                  <input className="form-control" id="name" type="text" placeholder="Your Name *" required="required" data-validation-required-message="Please enter your name."/>
-                  <p className="help-block text-danger"></p>
-                </div>
-                <div className="form-group">
-                  <input className="form-control" id="email" type="email" placeholder="Your Email *" required="required" data-validation-required-message="Please enter your email address."/>
-                  <p className="help-block text-danger"></p>
-                </div>
-                <div className="form-group">
-                  <input className="form-control" id="phone" type="tel" placeholder="Your Phone *" required="required" data-validation-required-message="Please enter your phone number."/>
-                  <p className="help-block text-danger"></p>
-                </div>
-              </div>
-              <div className="col-md-6">
-                <div className="form-group">
-                  <textarea className="form-control" id="message" placeholder="Your Message *" required="required" data-validation-required-message="Please enter a message."></textarea>
-                  <p className="help-block text-danger"></p>
-                </div>
-              </div>
-              <div className="clearfix"></div>
-              <div className="col-lg-12 text-center">
-                <div id="success"></div>
-                <button id="sendMessageButton" className="btn btn-primary btn-xl text-uppercase" type="submit">Send Message</button>
-              </div>
-            </div>
-          </form>
-        </div>
-      </div>
-    </div>
-  </section>
 
-  
-  <footer className="footer">
-    <div className="container">
-      <div className="row align-items-center">
-        <div className="col-md-4">
-          <span className="copyright">Copyright &copy; Your Website 2019</span>
-        </div>
-        <div className="col-md-4">
-          <ul className="list-inline social-buttons">
-            <li className="list-inline-item">
-              <a href="#something">
-                <i className="fa fa-twitter"></i>
-              </a>
-            </li>
-            <li className="list-inline-item">
-              <a href="#something">
-                <i className="fa fa-facebook-f"></i>
-              </a>
-            </li>
-            <li className="list-inline-item">
-              <a href="#something">
-                <i className="fa fa-linkedin-in"></i>
-              </a>
-            </li>
-          </ul>
-        </div>
-        <div className="col-md-4">
-          <ul className="list-inline quicklinks">
-            <li className="list-inline-item">
-              <a href="#something">Privacy Policy</a>
-            </li>
-            <li className="list-inline-item">
-              <a href="#something">Terms of Use</a>
-            </li>
-          </ul>
-        </div>
+        <button className="toggle-btn">
+          <FontAwesomeIcon icon={faBars} aria-hidden="true" />
+        </button>
       </div>
-    </div>
-  </footer>
+
+      <div className="left-side">
+        <div className="heading">
+          <h1>Discover the</h1>
+          <h2>Taste of Excellence</h2>
+        </div>
+        <p className="description">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Sunt maiores odit quaerat iure quisquam cupiditate sit itaque dignissimos nobis deleniti?</p>
+        <div className="total-price">
+          <h2>Total Order : </h2>
+          <h3>$22.35</h3>
+        </div>
+        <button>
+          <FontAwesomeIcon icon={faShoppingCart} aria-hidden="true" />
+          Buy Now
+        </button>
+      </div>
+
+      <div className="right-side">
+        <img src="assets/images/dish.png" alt="Dish" className="dish" />
+      </div>
     </div>
   );
-};
-//     <>
-//       <nav className="navbar navbar-expand-lg navbar-light bg-light">
-//         <Link className="navbar-brand" to="/">Maison City</Link>
-//         <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-//           <span className="navbar-toggler-icon"></span>
-//         </button>
-//         <div className="collapse navbar-collapse" id="navbarNav">
-//           <ul className="navbar-nav ml-auto">
-//             <li className="nav-item">
-//               <Link className="nav-link" to="/login">Login</Link>
-//             </li>
-//             <li className="nav-item">
-//               <Link className="nav-link" to="/register">Register</Link>
-//             </li>
-//           </ul>
-//         </div>
-//       </nav>
-//       <div className="jumbotron mt-5">
-//         <h1>Welcome to Maison City</h1>
-//         <p>Sign In and start building your maison list</p>
-//         <Link to="/login" className="btn btn-primary">
-//           Login
-//         </Link>
-//         <Link to="/register" className="btn btn-primary ml-3">
-//           Register
-//         </Link>
-//       </div>
-//     </>
-//   );
-// };
-
+}
 export default Landing;
