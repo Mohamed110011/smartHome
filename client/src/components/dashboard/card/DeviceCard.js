985a2bab31f54cb7bd141432109434ceede3821d
Mohamed <97177001+Mohamed110011@users.noreply.github.com>
2024-07-20 12:49:56 +0100

    RouterCard

--- client/src/components/dashboard/card/DeviceCard.js
+++ client/src/components/dashboard/card/DeviceCard.js
@@ -4,6 +4,7 @@ import SensorCard from './SensorCard';
 import LampCard from './LampCard';
 import CameraCard from './CameraCard';
 import RefrigeratorCard from './RefrigeratorCard';
+import RouterCard from './RouterCard';
 // Import other specific card components
 
 const DeviceCard = ({ device, fetchDevices }) => {
@@ -18,7 +19,9 @@ const DeviceCard = ({ device, fetchDevices }) => {
       case 'camera':
         return <CameraCard device={device} fetchDevices={fetchDevices} />;  
       case 'refrigerator':
-        return <RefrigeratorCard device={device} fetchDevices={fetchDevices} />;    
+        return <RefrigeratorCard device={device} fetchDevices={fetchDevices} />; 
+      case 'router':
+        return <RouterCard device={device} fetchDevices={fetchDevices} />;     
       // Add cases for other device types
       default:
         return (
